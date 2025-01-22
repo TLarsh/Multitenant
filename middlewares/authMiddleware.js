e3447dad926018a8cf530359d56e3e8d8651b052
ADEYEMI AYOBAMI SAMUEL <107284584+TLarsh@users.noreply.github.com>
2025-01-22 13:16:54 +0100

    Add files via upload
    commit changes

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,6 +1,7 @@
 // const User = require("../models/userModel");
 // const jwt = require("jsonwebtoken");
 // const asyncHandler = require("express-async-handler");
+// detenv = require('dotenv');
 
 
 // const authMiddleware = asyncHandler(async (req, res, next) => {
@@ -12,6 +13,12 @@
 //                 const decoded = jwt.verify(token, process.env.JWT_SECRET);
 //                 const user = await User.findById(decoded.id);
 //                 req.user = user;
+//                 // req.user = {
+//                 //     id: decoded.id,
+//                 //     name: decoded.name,
+//                 //     email: decoded.email,
+//                 //   };
+              
 //                 next();
 //             }
 //         }catch(error){
@@ -23,14 +30,13 @@
 // });
 
 // const isAdmin = asyncHandler(async (req, res, next) => {
-//     // console.log(req.user);
-//     const { email } = req.user;
-//     const isAdmin = await User.findOne({email});
-//     if (isAdmin.role !== "admin") {
-//         throw new Error("This is not an admin user");
-//     } else {
-//         next();
-//     }
+//     console.log(req.user);
+//     if (req.user && req.user.role==='admin') {
+//         return next(); // User is an admin, proceed to the next middleware or route handler
+//       }
+    
+//       // If not an admin, return an error
+//       return res.status(403).json({ error: "Access denied: Admins only" });
 // })
 
 // module.exports = { authMiddleware, isAdmin }
@@ -47,7 +53,9 @@ const authMiddleware = asyncHandler(async(req, res, next)=>{
         try{
             if(token){
                 const decoded = jwt.verify(token, process.env.JWT_SECRET); //verifying token
+                console.log(decoded)
                 const user = await User.findById(decoded.id);
+                console.log(user)
                 req.user = user; //Attach the decoded token payload to the request object
                 next();  //call the middleware / controller
             }
@@ -60,15 +68,16 @@ const authMiddleware = asyncHandler(async(req, res, next)=>{
 });
 
 const isAdmin = asyncHandler(async(req, res, next) =>{
+    console.log(req.user)
     const {email} = req.user;
     const adminUser = await User.findOne({email});
     if(adminUser.role !== "admin"){
-        throw new Error("You are not an admin")
+        throw new Error("Access denied: Admins only")
     } else {
         next();
     }
 
-})
+});
 
 module.exports = {authMiddleware, isAdmin}
 
