002fd50817c7db6a2dc47cfc4eaaa25ca6761415
ADEYEMI AYOBAMI SAMUEL <107284584+TLarsh@users.noreply.github.com>
2025-03-08 07:19:53 +0100

    Add files via upload
    remove default profile image

--- models/userModel.js
+++ models/userModel.js
@@ -50,8 +50,7 @@ var userSchema = new mongoose.Schema({
         default:"client",
     },
     profileImage:{
-        type:String,
-        default:"https://res.cloudinary.com/dxe5jvtit/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1741298729/qxu3use3eu1i0xstjdjx.jpg"
+        type:String
     },
     isActive:{
         type:Boolean,
